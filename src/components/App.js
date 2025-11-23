5e29f0074bf96b26ae17ab45a544562b85b34075
madhur <madhurpanghal0001@gmail.com>
2025-11-23 21:57:44 +0530

    third

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 
 import React from "react";
 import './../styles/App.css';
-import PageLayout from "./PageLayout";
+
+
+const PageLayout = ({header, children, footer}) => {
+
+  return (
+    <div>
+      <h1>{header}</h1>
+      <p>{children}</p>
+      <p>{footer}</p>
+    </div>
+  )
+}
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
         <PageLayout 
-          header='Welcome to my Website'
+          header='Welcome to my website'
           children='This is the content of my website.'
           footer='© 2023 My Website.All rights reserved.'
         />
